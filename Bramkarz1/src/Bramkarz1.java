c609ee4b7449d2e5c13f63b7691edbdaa2cd48a1
dorota <dorotadowz68@gmail.com>
2022-09-04 14:21:56 +0200

    Add new project Bramkarz

---
+++ Bramkarz1/src/Bramkarz1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Bramkarz1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] nameList = new String[]{"Anna", "Darek", "Piotr", "Dawid"};
+        boolean flag = false;
+        boolean again = false;
+        do {
+            System.out.println("Jak masz na imie?");
+            String name = scanner.next();
+            for (String n : nameList) {
+                if (name.equals(n)) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println("Jestes na liscie mozesz wejsc do Zoo");
+            } else {
+                System.out.println("Nie ma cie na liscie, placisz 10zl");
+            }
+
+            System.out.println("Czy jest ktos nastepny? tak lub nie");
+            String odpowiedz = scanner.next();
+            if (odpowiedz.equals("tak")) {
+                again = true;
+            }
+        } while (again);
+    }
+}
